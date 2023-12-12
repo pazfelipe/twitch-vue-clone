e922baadb7d43e815a17c8c38aa5d32f0cee3aa6
Felipe Paz <xxfelipepaz@gmail.com>
2023-12-12 10:07:41 -0300

    feat: create default layout

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,27 @@
+<script setup>
+const colorMode = useColorMode()
+
+const isDark = ref(true)
+
+watch(isDark, () => {
+  if (isDark.value) {
+    colorMode.preference = 'dark'
+  } else {
+    colorMode.preference = 'light'
+  }
+})
+
+</script>
+
+<template>
+  <div>
+    <input type="checkbox" v-model="isDark" class="checkbox" />
+    <NuxtPage class="mx-auto p-4" />
+  </div>
+</template>
+
+<style lang="scss">
+body {
+  @apply min-h-screen bg-white dark:bg-gray-800 dark:text-gray-200;
+}
+</style>
